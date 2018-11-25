1ab0e0baf51b78451aa08bff242eb8a76db5fa0c
wxf <997245606@qq.com>
2018-11-25 23:31:30 +0800

    login page optimization

--- NuistOJv5_FE/src/pages/login/views/Login.vue
+++ NuistOJv5_FE/src/pages/login/views/Login.vue
@@ -5,29 +5,32 @@
             <h3 class="form-signin-heading">Please login!</h3>
             <div class="input-group mb-3">
               <div class="input-group-prepend">
-                <span class="input-group-text span-icon" id="basic-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
+                <span class="input-group-text span-icon" id="bind-username"><i class="fa fa-user" aria-hidden="true"></i></span>
               </div>
               <label for="username" class="sr-only">Username</label>
               <input type="text" name="username" id="form-username" class="form-control" placeholder="Username..."
-              required aria-label="Username" aria-describedby="basic-addon1" autofocus>
+              required aria-label="Username" aria-describedby="bind-username" autofocus>
             </div>
             <div class="input-group mb-3">
               <div class="input-group-prepend">
-                <span class="input-group-text span-icon" id="basic-addon2"><i class="fa fa-lock" aria-hidden="true"></i></span>
+                <span class="input-group-text span-icon" id="bind-password"><i class="fa fa-lock" aria-hidden="true"></i></span>
               </div>
               <label for="inputPassword" class="sr-only">Password</label>
-              <input type="password" name="password" id="form-password" class="form-control" placeholder="Password..." required aria-label="Password" aria-describedby="basic-addon2">
+              <input type="password" name="password" id="form-password" class="form-control" placeholder="Password..." required aria-label="Password" aria-describedby="bind-password">
             </div>
-            <div>
-
-                <div class="input-group">
-                    <label for="verifyCode">
-                        点击图片刷新验证码
-                        <img id="verify" src="../../../assets/logo.png">
-                    </label>
-                    <input type="text" class="form-control" id="verifyCode" name="verifyCode" placeholder="四位字符验证码" autocomplete="off">
+                <div class="input-group mb-3"  style="margin-bottom:10px">
+                   <div class="input-group-prepend">
+                      <span class="input-group-text span-icon" id="bind-veriycode"><i class="fa fa-lightbulb-o" aria-hidden="true"></i></span>
+                   </div>
+                    <div>
+                        <input type="text" class="form-control" aria-label="VeryCode" aria-describedby="bind-veriycode" id="verifyCode" name="verifyCode" placeholder="四位字符验证码" autocomplete="off">
+                    </div>
+                    <div>
+                        <label for="verifyCode">
+                             <img id="verify" style="width:50px;height:40px;" src="../../../assets/logo.png">
+                        </label>
+                    </div>
                 </div>
-            </div>
             <button id="btn_login" class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
         </form>
       <div style="text-align:center;">
