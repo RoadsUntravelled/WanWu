49b23f497ab0a49b11f294860a114da1bb998b79
wxf <997245606@qq.com>
2019-01-24 03:11:00 +0800

    iu

--- WanWu_FE/src/pages/main/index.js
+++ WanWu_FE/src/pages/main/index.js
@@ -4,19 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'jquery'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import './css/login.css'
 import 'font-awesome-webpack'
 
+import ElementUI from 'element-ui'
+import './element-#EEECE0/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+Vue.use(ElementUI)
+Vue.use(iView)
 Vue.config.productionTip = false
 console.log(router)
 
-/* eslint-disable no-new */
-// eslint-disable-next-line
 /* eslint-disable */
+// eslint-disable-next-line
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
--- WanWu_FE/src/pages/main/views/Main.vue
+++ WanWu_FE/src/pages/main/views/Main.vue
@@ -1,67 +1,30 @@
 <template>
-    <div class="container">
-        <form id="form_login" class="form-login" role="form" action="" method="post">
-            <h1><strong>TSOJ</strong></h1>
-            <h3 class="form-signin-heading">Please login!</h3>
-            <IconInput bind_id="bind-username" icon_class="fa fa-user">
-              <label for="form-username" class="sr-only">Username</label>
-              <input v-model="formLogin.username" type="text" name="username" id="form-username" class="form-control" placeholder="Username..."
-              required aria-label="Username" aria-describedby="bind-username" autofocus>
-            </IconInput>
-            <IconInput bind_id="bind-password" icon_class="fa fa-lock">
-              <label for="form-password" class="sr-only">Password</label>
-              <input v-model="formLogin.password" type="password" name="password" id="form-password" class="form-control" placeholder="Password..." required aria-label="Password" aria-describedby="bind-password">
-            </IconInput>
-            <IconInput bind_id="bind-veriycode" icon_class="fa fa-lightbulb-o">
-                <div>
-                  <input v-model="formLogin.verifyCode" type="text" class="form-control" aria-label="VeryCode" aria-describedby="bind-veriycode" id="verifyCode" name="verifyCode" placeholder="四位字符验证码" autocomplete="off">
-                </div>
-                <div>
-                  <label for="verifyCode">
-                    <img id="verify" style="width:50px;height:40px;" src="../../../assets/logo.png">
-                  </label>
-                </div>
-            </IconInput>
-            <button id="btn_login" class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-        </form>
-      <div style="text-align:center;">
-          <router-link to="#" >竞赛登录</router-link>
-      </div>
-      <div style="text-align:center;">
-          <router-link to="#" >下载中心</router-link>
-      </div>
-    </div>
+<el-container>
+  <el-header>
+  </el-header>
+  <el-aside>
+  </el-aside>
+  <el-main>
+  </el-main>
+  <el-footer>
+  </el-footer>
+</el-container>
 </template>
 
 <script>
+// eslint-disable-next-line
 /* eslint-disable */
-import IconInput from "@main/components/IconInput"
+//import IconInput from "@main/components/IconInput"
 export default {
   name: 'Main',
-  components: {IconInput},
+  //components: {IconInput},
   data () {
     return {
-      formLogin: {
-        username: '',
-        password: '',
-        verifyCode: ''
-      }
     }
   }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-        .captcha {
-            border-radius: 2px;
-            cursor: pointer;
-            position: absolute;
-            z-index: 3;
-            left: 0;
-            top: 0;
-        }
 
-        .verifyCodes {
-            padding-left: 0px;
-        }
 </style>
